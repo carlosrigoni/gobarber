cc198fb4235cb34ff2623e556cf5abb950280bf2
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-07-05 16:34:57 -0300

    Debugando NodeJS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-  response.json({ message: 'Hello gostack' }),
-);
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
+  console.log('🚀 Server started on port 3333!');
 });
