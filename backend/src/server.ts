fea9b489a4948c055df14e5caa796dea5b63b123
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-07-05 10:15:35 -0300

    designer patterns Eslint, Prettier & EditorConfig

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
-import express, { request } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello gostack' })
-})
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello gostack' }),
+);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333')
-})
+  console.log('🚀 Server started on port 3333');
+});
