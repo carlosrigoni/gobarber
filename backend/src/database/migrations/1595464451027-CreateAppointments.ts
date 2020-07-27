4c70e001ffeae0740fc57ebeaab487f60a69cb3b
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-07-27 20:23:18 -0300

    Repositório do TypeORM

--- backend/src/database/migrations/1595464451027-CreateAppointments.ts
+++ backend/src/database/migrations/1595464451027-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1595464451027
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
