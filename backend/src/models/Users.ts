15c9fb6adc82b4274748fac164c91a16f75b5b95
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-07-29 19:50:44 -0300

    Model e migration de usuários

--- backend/src/models/Users.ts
+++ backend/src/models/Users.ts
@@ -7,16 +7,19 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider_id: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -25,4 +28,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
