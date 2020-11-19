6fe0232ac102662363070ed3dc754a7a0ddfc567
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-11-19 14:57:25 -0300

    Criando o primeiro teste

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/en/configuration.html
  */
 
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -81,7 +83,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- backend/src/modules/apointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/apointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,20 @@
-import CreateAppointmentService from 'CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345678');
   });
 
   // it('should not be able to create two appointments at the same date', () => {
