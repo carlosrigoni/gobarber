34bd85b9a202a9085c75d8756f2519acce28558f
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-11-19 21:55:26 -0300

    Testes de agendamento

--- backend/src/modules/apointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ backend/src/modules/apointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import { uuid } from 'uuidv4';
+import { isEqual } from 'date-fns';
 
 import IAppointmentRepository from '@modules/apointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/apointments/dtos/ICreateAppointment.dto';
@@ -9,8 +10,8 @@ class AppointmentsRepository implements IAppointmentRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(
-      appointment => appointment.date === date,
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
     );
 
     return findAppointment;
